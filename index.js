6b24b462fe87417458447b890e019a734b3f81b3
Pearl Jones <jonesatlakota@yahoo.com>
2023-07-29 19:57:17 -0500

    Starter code

---
+++ index.js
@@ -0,0 +1,46 @@
+// this index.js if for the ReadMe Generator project
+// this includes installing NodeJS Inquirer
+
+// require the nodejs files
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// the filename to use is specified here
+var filename = 'logo.svg';
+
+// Ask user information to put in the README file
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'Enter up to 3 characters for your logo  ',
+            name: 'charEl',
+        },
+        {
+            type: 'input',
+            message: 'What color would your like your text to be? ',
+            name: 'textColorEl',
+        },
+        {
+            type: 'list',
+            message: 'What is your preferred logo shape?',
+            name: 'shapeEl',
+            choices: ['circle', 'triangle', 'square'],
+          },
+        {
+            type: 'input',
+            message: 'What color would your like your shape to be? ',
+            name: 'shapeColorEl',
+        },
+    ])
+    .then((data) =>
+    
+    //the actual call to write the user input to the README.md file
+    {
+        fs.writeFile(filename,JSON.stringify(data, null, '\t'), (err) =>
+          err ? console.log(err) : console.log('Generated logo.svg')
+        );
+    }
+    );
+
